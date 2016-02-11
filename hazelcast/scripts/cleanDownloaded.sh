57ad2bee74efa5dd8892b0c7d77ded85ee68a238
sancar <sancar@hazelcast.com>
2016-02-11 15:00:21 +0200

    add git repo option to maven for easier configuration when releasing. Added a fix for tag names like v3.6 v3.5. Removed some unused dependencies.

--- hazelcast/scripts/cleanDownloaded.sh
+++ hazelcast/scripts/cleanDownloaded.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 HAZELCAST_BRANCH="$1"
-rm -rf hazelcast-$HAZELCAST_BRANCH
 rm $HAZELCAST_BRANCH.zip*
+
+if([[ $HAZELCAST_BRANCH == v* ]])
+then
+   HAZELCAST_BRANCH=${HAZELCAST_BRANCH:1}
+fi
+
+rm -rf hazelcast-$HAZELCAST_BRANCH
 rm -rf ../hazelcast/downloaded/
--- hazelcast/scripts/downloadHazelcast.sh
+++ hazelcast/scripts/downloadHazelcast.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-HAZELCAST_BRANCH="$1"
-wget -q https://github.com/hazelcast/hazelcast/archive/$HAZELCAST_BRANCH.zip
+HAZELCAST_REPO="$1"
+HAZELCAST_BRANCH="$2"
+
+wget -q https://github.com/$HAZELCAST_REPO/hazelcast/archive/$HAZELCAST_BRANCH.zip
 unzip -oq $HAZELCAST_BRANCH.zip
+
+if([[ $HAZELCAST_BRANCH == v* ]])
+then
+   HAZELCAST_BRANCH=${HAZELCAST_BRANCH:1}
+fi
+
 cp -R ./hazelcast-$HAZELCAST_BRANCH/hazelcast/src/main ../hazelcast/downloaded/
