883b29dffe0d8312b02fb3fa7fef1667954fc187
ihsan demir <ihsan@hazelcast.com>
2016-07-15 11:17:07 +0300

    Made Condition await and beforeAwait retryable since it is using reference id now.

--- hazelcast/src/main/java/com/hazelcast/client/impl/protocol/template/ConditionCodecTemplate.java
+++ hazelcast/src/main/java/com/hazelcast/client/impl/protocol/template/ConditionCodecTemplate.java
@@ -34,7 +34,7 @@ public interface ConditionCodecTemplate {
      * @param referenceId The client-wide unique id for this request. It is used to make the request idempotent by sending the same reference id during retries.
      * @return False if the waiting time detectably elapsed before return from the method, else true
      */
-    @Request(id = 1, retryable = false, response = ResponseMessageConst.BOOLEAN, partitionIdentifier = "lockName")
+    @Request(id = 1, retryable = true, response = ResponseMessageConst.BOOLEAN, partitionIdentifier = "lockName")
     Object await(String name, long threadId, long timeout, String lockName, @Since(value = "1.2") long referenceId);
 
     /**
@@ -60,7 +60,7 @@ public interface ConditionCodecTemplate {
      * @param lockName Name of the lock to wait on.
      * @param referenceId The client-wide unique id for this request. It is used to make the request idempotent by sending the same reference id during retries.
      */
-    @Request(id = 2, retryable = false, response = ResponseMessageConst.VOID, partitionIdentifier = "lockName")
+    @Request(id = 2, retryable = true, response = ResponseMessageConst.VOID, partitionIdentifier = "lockName")
     void beforeAwait(String name, long threadId, String lockName, @Since(value = "1.2") long referenceId);
 
     /**
