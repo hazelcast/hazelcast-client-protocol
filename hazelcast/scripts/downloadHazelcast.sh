e3e3fc593508a2d120db1e1be7a59476d7caf1ae
Ahmet Mırçık <ahmetmircik@gmail.com>
2016-12-02 18:55:56 +0300

    Fixed branch name extraction for branch names with slash like `fix/3.8/a`

--- hazelcast/scripts/downloadHazelcast.sh
+++ hazelcast/scripts/downloadHazelcast.sh
@@ -2,13 +2,15 @@
 
 HAZELCAST_REPO="$1"
 HAZELCAST_BRANCH="$2"
+DOWNLOADED_HAZELCAST_BRANCH=${2##*/}    # finds downloaded-file-name by extracting string after rightmost slash.(e.g. extracted name from `fix/3.8/abc` will be `abc`)
+COPY_HAZELCAST_BRANCH=${2//\//-}    # finds unzipped directory name by replacing `/` with `-` (e.g. `fix/3.8/abc` will become `fix-3.8-abc`)
 
 wget -q https://github.com/$HAZELCAST_REPO/hazelcast/archive/$HAZELCAST_BRANCH.zip
-unzip -oq $HAZELCAST_BRANCH.zip
+unzip -oq $DOWNLOADED_HAZELCAST_BRANCH.zip
 
-if([[ $HAZELCAST_BRANCH == v* ]])
+if([[ $DOWNLOADED_HAZELCAST_BRANCH == v* ]])
 then
-   HAZELCAST_BRANCH=${HAZELCAST_BRANCH:1}
+   DOWNLOADED_HAZELCAST_BRANCH=${DOWNLOADED_HAZELCAST_BRANCH:1}
 fi
 
-cp -R ./hazelcast-$HAZELCAST_BRANCH/hazelcast/src/main ../hazelcast/downloaded/
+cp -R ./hazelcast-${COPY_HAZELCAST_BRANCH}/hazelcast/src/main ../hazelcast/downloaded/
