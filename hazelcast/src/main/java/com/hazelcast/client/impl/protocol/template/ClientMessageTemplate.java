1ad39565ea780aa299c544d36c1a994b25aa6ae0
ihsan demir <ihsan@hazelcast.com>
2017-05-30 12:25:41 +0300

    Updated client statistics codec javadoc. Described each statistic. Changed "nearcache." to "nc." in the statistics key descriptions.

--- hazelcast/src/main/java/com/hazelcast/client/impl/protocol/template/ClientMessageTemplate.java
+++ hazelcast/src/main/java/com/hazelcast/client/impl/protocol/template/ClientMessageTemplate.java
@@ -187,56 +187,126 @@ public interface ClientMessageTemplate {
      *
      * An e.g. Operating system committedVirtualMemorySize path would be: os.committedVirtualMemorySize
      *
+     * Please note that if any client implementation can not provide the value for a statistics, the corresponding key, valaue
+     * pair will not be presented in the statistics string. Only the ones, that the client can provide will be added.
+     *
      * The statistics key names can be one of the following (Used IMap named <StatIMapName> and ICache Named
      * <StatICacheName> and assuming that the near cache is configured):
      *
-     * clientType
-     * clusterConnectionTimestamp
-     * credentials.principal
-     * clientAddress
-     * clientName
-     * enterprise
-     * lastStatisticsCollectionTime
-     * nearcache.<StatIMapName>.creationTime
-     * nearcache.<StatIMapName>.evictions
-     * nearcache.<StatIMapName>.expirations
-     * nearcache.<StatIMapName>.hits
-     * nearcache.<StatIMapName>.lastPersistenceDuration
-     * nearcache.<StatIMapName>.lastPersistenceFailure
-     * nearcache.<StatIMapName>.lastPersistenceKeyCount
-     * nearcache.<StatIMapName>.lastPersistenceTime
-     * nearcache.<StatIMapName>.lastPersistenceWrittenBytes
-     * nearcache.<StatIMapName>.misses
-     * nearcache.<StatIMapName>.ownedEntryCount
-     * nearcache.<StatIMapName>.ownedEntryMemoryCost
-     * nearcache.hz/<StatICacheName>.creationTime
-     * nearcache.hz/<StatICacheName>.evictions
-     * nearcache.hz/<StatICacheName>.expirations
-     * nearcache.hz/<StatICacheName>.hits
-     * nearcache.hz/<StatICacheName>.lastPersistenceDuration
-     * nearcache.hz/<StatICacheName>.lastPersistenceFailure
-     * nearcache.hz/<StatICacheName>.lastPersistenceKeyCount
-     * nearcache.hz/<StatICacheName>.lastPersistenceTime
-     * nearcache.hz/<StatICacheName>.lastPersistenceWrittenBytes
-     * nearcache.hz/<StatICacheName>.misses
-     * nearcache.hz/<StatICacheName>.ownedEntryCount
-     * nearcache.hz/<StatICacheName>.ownedEntryMemoryCost
-     * os.committedVirtualMemorySize
-     * os.freePhysicalMemorySize
-     * os.freeSwapSpaceSize
-     * os.maxFileDescriptorCount
-     * os.openFileDescriptorCount
-     * os.processCpuTime
-     * os.systemLoadAverage
-     * os.totalPhysicalMemorySize
-     * os.totalSwapSpaceSize
-     * runtime.availableProcessors
-     * runtime.freeMemory
-     * runtime.maxMemory
-     * runtime.totalMemory
-     * runtime.uptime
-     * runtime.usedMemory
-     * userExecutor.queueSize
+     * clientType: The string that represents the client type. See {@link com.hazelcast.core.ClientType}
+     *
+     * clusterConnectionTimestamp: The time that the client connected to the cluster (milliseconds since epoch). It is reset on
+     * each reconnection.
+     *
+     * credentials.principal: The principal of the client if it exists. For
+     * {@link com.hazelcast.security.UsernamePasswordCredentials}, this is the username, for custom authentication it is set by
+     * the {@link com.hazelcast.security.Credentials} implementer.
+     *
+     * clientAddress: The address of the client. It is formatted as "<IP>:<port>"
+     *
+     * clientName: The name of the client instance. See ClientConfig.setInstanceName.
+     *
+     * enterprise: "true" if the client is an enterprise client, "false" otherwise.
+     *
+     * lastStatisticsCollectionTime: The time stamp (milliseconds since epoch) when the latest update for the statistics is
+     * collected.
+     *
+     * Near cache statistics (see {@link com.hazelcast.monitor.NearCacheStats}):
+     *
+     * nc.<StatIMapName>.creationTime: The creation time (milliseconds since epoch) of this Near Cache on the client.
+     *
+     * nc.<StatIMapName>.evictions: The number of evictions of Near Cache entries owned by this client.
+     *
+     * nc.<StatIMapName>.expirations: The number of TTL and max-idle expirations of Near Cache entries owned by the client.
+     *
+     * nc.<StatIMapName>.hits: The number of hits (reads) of Near Cache entries owned by the client.
+     *
+     * nc.<StatIMapName>.lastPersistenceDuration: The duration in milliseconds of the last Near Cache key persistence
+     * (when the pre-load feature is enabled).
+     *
+     * nc.<StatIMapName>.lastPersistenceFailure: The failure reason of the last Near Cache persistence (when the pre-load
+     * feature is enabled).
+     *
+     * nc.<StatIMapName>.lastPersistenceKeyCount: The number of Near Cache key persistences (when the pre-load feature is
+     * enabled).
+     *
+     * nc.<StatIMapName>.lastPersistenceTime: The timestamp (milliseconds since epoch) of the last Near Cache key
+     * persistence (when the pre-load feature is enabled).
+     *
+     * nc.<StatIMapName>.lastPersistenceWrittenBytes: The written number of bytes of the last Near Cache key persistence
+     * (when the pre-load feature is enabled).
+     *
+     * nc.<StatIMapName>.misses: The number of misses of Near Cache entries owned by the client.
+     *
+     * nc.<StatIMapName>.ownedEntryCount: the number of Near Cache entries owned by the client.
+     *
+     * nc.<StatIMapName>.ownedEntryMemoryCost: Memory cost (number of bytes) of Near Cache entries owned by the client.
+     *
+     * nc.hz/<StatICacheName>.creationTime: The creation time of this Near Cache on the client.
+     *
+     * nc.hz/<StatICacheName>.evictions: The number of evictions of Near Cache entries owned by the client.
+     *
+     * nc.hz/<StatICacheName>.expirations: The number of TTL and max-idle expirations of Near Cache entries owned by the
+     * client.
+     *
+     * nc.hz/<StatICacheName>.hits
+     * nc.hz/<StatICacheName>.lastPersistenceDuration
+     * nc.hz/<StatICacheName>.lastPersistenceFailure
+     * nc.hz/<StatICacheName>.lastPersistenceKeyCount
+     * nc.hz/<StatICacheName>.lastPersistenceTime
+     * nc.hz/<StatICacheName>.lastPersistenceWrittenBytes
+     * nc.hz/<StatICacheName>.misses
+     * nc.hz/<StatICacheName>.ownedEntryCount
+     * nc.hz/<StatICacheName>.ownedEntryMemoryCost
+     *
+     * Operating System Statistics (see {@link com.hazelcast.internal.metrics.metricsets.OperatingSystemMetricSet},
+     * {@link sun.management.OperatingSystemImpl}) and {@link com.sun.management.UnixOperatingSystemMXBean}:
+     *
+     * os.committedVirtualMemorySize: The amount of virtual memory that is guaranteed to be available to the running process in
+     * bytes, or -1 if this operation is not supported.
+     *
+     * os.freePhysicalMemorySize: The amount of free physical memory in bytes.
+     *
+     * os.freeSwapSpaceSize: The amount of free swap space in bytes.
+     *
+     * os.maxFileDescriptorCount: The maximum number of file descriptors.
+     *
+     * os.openFileDescriptorCount: The number of open file descriptors.
+     *
+     * os.processCpuTime: The CPU time used by the process in nanoseconds.
+     *
+     * os.systemLoadAverage: The system load average for the last minute. (See
+     * {@link java.lang.management.OperatingSystemMXBean#getSystemLoadAverage})
+     * The system load average is the sum of the number of runnable entities
+     * queued to the {@link java.lang.management.OperatingSystemMXBean#getAvailableProcessors} available processors
+     * and the number of runnable entities running on the available processors
+     * averaged over a period of time.
+     * The way in which the load average is calculated is operating system
+     * specific but is typically a damped time-dependent average.
+     * <p>
+     * If the load average is not available, a negative value is returned.
+     * <p>
+     *
+     * os.totalPhysicalMemorySize: The total amount of physical memory in bytes.
+     *
+     * os.totalSwapSpaceSize: The total amount of swap space in bytes.
+     *
+     * Runtime statistics (See {@link Runtime}:
+     *
+     * runtime.availableProcessors: The number of processors available to the process.
+     *
+     * runtime.freeMemory: an approximation to the total amount of memory currently available for future allocated objects,
+     * measured in bytes.
+     *
+     * runtime.maxMemory: The maximum amount of memory that the process will  attempt to use, measured in bytes
+     *
+     * runtime.totalMemory: The total amount of memory currently available for current and future objects, measured in bytes.
+     *
+     * runtime.uptime: The uptime of the process in milliseconds.
+     *
+     * runtime.usedMemory: The difference of total memory and used memory in bytes.
+     *
+     * userExecutor.queueSize: The number of waiting tasks in the client user executor (See ClientExecutionService#getUserExecutor)
      *
      * Not: Please observe that the name for the ICache appears to be the hazelcast instance name "hz" followed by "/" and
      * followed by the cache name provided which is StatICacheName.
@@ -249,16 +319,16 @@ public interface ClientMessageTemplate {
      * runtime.usedMemory=23234760,runtime.freeMemory=108820280,os.totalSwapSpaceSize=5368709120,runtime.availableProcessors=4,
      * runtime.uptime=62181,os.committedVirtualMemorySize=4144533504,os.maxFileDescriptorCount=10240,
      * runtime.totalMemory=132055040,os.processCpuTime=6320000000,os.openFileDescriptorCount=67,os.freeSwapSpaceSize=1102053376,
-     * nearcache.StatIMapName.creationTime=1496047097216,nearcache.StatIMapName.evictions=0,nearcache.StatIMapName.hits=1,
-     * nearcache.StatIMapName.lastPersistenceDuration=0,nearcache.StatIMapName.lastPersistenceKeyCount=0,
-     * nearcache.StatIMapName.lastPersistenceTime=0,nearcache.StatIMapName.lastPersistenceWrittenBytes=0,
-     * nearcache.StatIMapName.misses=1,nearcache.StatIMapName.ownedEntryCount=1,nearcache.StatIMapName.expirations=0,
-     * nearcache.StatIMapName.ownedEntryMemoryCost=140,nearcache.hz/StatICacheName.creationTime=1496047103690,
-     * nearcache.hz/StatICacheName.evictions=0,nearcache.hz/StatICacheName.hits=1,
-     * nearcache.hz/StatICacheName.lastPersistenceDuration=0,nearcache.hz/StatICacheName.lastPersistenceKeyCount=0,
-     * nearcache.hz/StatICacheName.lastPersistenceTime=0,nearcache.hz/StatICacheName.lastPersistenceWrittenBytes=0,
-     * nearcache.hz/StatICacheName.misses=1,nearcache.hz/StatICacheName.ownedEntryCount=1,
-     * nearcache.hz/StatICacheName.expirations=0,nearcache.hz/StatICacheName.ownedEntryMemoryCost=140
+     * nc.StatIMapName.creationTime=1496047097216,nc.StatIMapName.evictions=0,nc.StatIMapName.hits=1,
+     * nc.StatIMapName.lastPersistenceDuration=0,nc.StatIMapName.lastPersistenceKeyCount=0,
+     * nc.StatIMapName.lastPersistenceTime=0,nc.StatIMapName.lastPersistenceWrittenBytes=0,
+     * nc.StatIMapName.misses=1,nc.StatIMapName.ownedEntryCount=1,nc.StatIMapName.expirations=0,
+     * nc.StatIMapName.ownedEntryMemoryCost=140,nc.hz/StatICacheName.creationTime=1496047103690,
+     * nc.hz/StatICacheName.evictions=0,nc.hz/StatICacheName.hits=1,
+     * nc.hz/StatICacheName.lastPersistenceDuration=0,nc.hz/StatICacheName.lastPersistenceKeyCount=0,
+     * nc.hz/StatICacheName.lastPersistenceTime=0,nc.hz/StatICacheName.lastPersistenceWrittenBytes=0,
+     * nc.hz/StatICacheName.misses=1,nc.hz/StatICacheName.ownedEntryCount=1,
+     * nc.hz/StatICacheName.expirations=0,nc.hz/StatICacheName.ownedEntryMemoryCost=140
      *
      *
      * @param stats The key=value pairs separated by the ',' character
