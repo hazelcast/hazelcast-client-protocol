f702545c9afcecbd127c6675d36d07880079e433
donnerbart <github@thunderphreak.de>
2017-08-30 16:12:03 +0200

    Fixed includes for sources and JavaDoc JAR
    * fixed the include settings for the Maven sources and JavaDoc plugin
    * added a debug file which prints which Hazelcast version was downloaded
    * small cleanup of pom.xml files
    
    (cherry picked from commit 4bae497)

--- hazelcast/scripts/cleanDownloaded.sh
+++ hazelcast/scripts/cleanDownloaded.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-HAZELCAST_BRANCH="${1//\//-}"   # finds directory name by replacing `/` with `-` (e.g. `fix/3.8/abc` will become `fix-3.8-abc`)
-rm $HAZELCAST_BRANCH.zip*
+# finds directory name by replacing `/` with `-` (e.g. `fix/3.8/abc` will become `fix-3.8-abc`)
+HAZELCAST_BRANCH="${1//\//-}"
+rm ${HAZELCAST_BRANCH}.zip*
 
-if([[ $HAZELCAST_BRANCH == v* ]])
-then
+if ([[ ${HAZELCAST_BRANCH} == v* ]]); then
    HAZELCAST_BRANCH=${HAZELCAST_BRANCH:1}
 fi
 
-rm -rf hazelcast-$HAZELCAST_BRANCH
+rm -rf hazelcast-${HAZELCAST_BRANCH}
 rm -rf ../hazelcast/downloaded/
