f73af7e45ed6b61799362e0a1875f43eca6fa9bf
Donnerbart <github@thunderphreak.de>
2017-09-11 11:46:25 +0200

    Fixed includes for sources and JavaDoc JAR (#100)
    * fixed the include settings for the Maven sources and JavaDoc plugin
    * added a debug file which prints which Hazelcast version was downloaded
    * small cleanup of pom.xml files
    
    (cherry picked from commit 4bae497)

--- hazelcast/scripts/downloadHazelcast.sh
+++ hazelcast/scripts/downloadHazelcast.sh
@@ -2,15 +2,21 @@
 
 HAZELCAST_REPO="$1"
 HAZELCAST_BRANCH="$2"
-DOWNLOADED_HAZELCAST_BRANCH=${2##*/}    # finds downloaded-file-name by extracting string after rightmost slash.(e.g. extracted name from `fix/3.8/abc` will be `abc`)
-COPY_HAZELCAST_BRANCH=${2//\//-}    # finds unzipped directory name by replacing `/` with `-` (e.g. `fix/3.8/abc` will become `fix-3.8-abc`)
+# finds downloaded-file-name by extracting string after rightmost slash.(e.g. extracted name from `fix/3.8/abc` will be `abc`)
+DOWNLOADED_HAZELCAST_BRANCH=${2##*/}
+# finds unzipped directory name by replacing `/` with `-` (e.g. `fix/3.8/abc` will become `fix-3.8-abc`)
+COPY_HAZELCAST_BRANCH=${2//\//-}
 
-wget -q https://github.com/$HAZELCAST_REPO/hazelcast/archive/$HAZELCAST_BRANCH.zip
-unzip -oq $DOWNLOADED_HAZELCAST_BRANCH.zip
+wget -q https://github.com/${HAZELCAST_REPO}/hazelcast/archive/${HAZELCAST_BRANCH}.zip
+unzip -oq ${DOWNLOADED_HAZELCAST_BRANCH}.zip
 
-if([[ $DOWNLOADED_HAZELCAST_BRANCH == v* ]])
-then
+if ([[ ${DOWNLOADED_HAZELCAST_BRANCH} == v* ]]); then
    DOWNLOADED_HAZELCAST_BRANCH=${DOWNLOADED_HAZELCAST_BRANCH:1}
 fi
+
+echo "HAZELCAST_REPO: $HAZELCAST_REPO" > download.info
+echo "HAZELCAST_BRANCH: $HAZELCAST_BRANCH" >> download.info
+echo "DOWNLOADED_HAZELCAST_BRANCH: $DOWNLOADED_HAZELCAST_BRANCH" >> download.info
+echo "COPY_HAZELCAST_BRANCH: $DOWNLOADED_HAZELCAST_BRANCH" >> download.info
 
 cp -R ./hazelcast-${COPY_HAZELCAST_BRANCH}/hazelcast/src/main ../hazelcast/downloaded/
