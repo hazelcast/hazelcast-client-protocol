f18d1ff68a1dae2749a809c3a947785b10008a11
sancar <sancar@hazelcast.com>
2019-03-28 12:25:50 +0300

    Fix clean and download behaviour (#179)
    Clean was searching for a specific name and causing leftovers when
    branch has changed. It will be downloaded to tempDownloaded directory
    and all directory will be removed.
    
    Download was working twice when mvn install is called. It is fixed
    with <phase>DISABLE_FORKED_LIFECYCLE_MSOURCES-13</phase>

--- hazelcast/scripts/cleanDownloaded.sh
+++ hazelcast/scripts/cleanDownloaded.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 
-# finds directory name by replacing `/` with `-` (e.g. `fix/3.8/abc` will become `fix-3.8-abc`)
-HAZELCAST_BRANCH="${1//\//-}"
-rm ${HAZELCAST_BRANCH}.zip*
-
-if ([[ ${HAZELCAST_BRANCH} == v* ]]); then
-   HAZELCAST_BRANCH=${HAZELCAST_BRANCH:1}
-fi
-
-rm -rf hazelcast-${HAZELCAST_BRANCH}
+rm -rf ./tempDownloaded
 rm -rf ../hazelcast/downloaded/
