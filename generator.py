c5d6a010078b085e8804f253df6ec935732d1662
mdumandag <metin@hazelcast.com>
2019-08-02 12:48:57 +0300

    add initial support for other languages

--- generator.py
+++ generator.py
@@ -3,36 +3,9 @@
 import time
 import argparse
 
-from enum import Enum
 from util import *
 
-
-class SupportedLanguages(Enum):
-    JAVA = 'java'
-    CPP = 'cpp'
-    CS = 'cs'
-    PY = 'py'
-    NODE = 'node'
-    GO = 'go'
-
-
-output_directories = {
-    SupportedLanguages.JAVA: '/hazelcast/src/main/java/com/hazelcast/client/impl/protocol/codec/',
-    SupportedLanguages.CPP: '/hazelcast/generated-sources/src/hazelcast/client/protocol/codec/',
-    SupportedLanguages.CS: '/Hazelcast.Net/Hazelcast.Client.Protocol.Codec/',
-    SupportedLanguages.PY: '/hazelcast/protocol/codec/',
-    SupportedLanguages.NODE: '/src/codec/',
-    SupportedLanguages.GO: '/internal/proto/'
-}
-
-file_extensions = {
-    SupportedLanguages.JAVA: 'java',
-    SupportedLanguages.CPP: 'cpp',  # TODO header files ?
-    SupportedLanguages.CS: 'cs',
-    SupportedLanguages.PY: 'py',
-    SupportedLanguages.NODE: 'ts',
-    SupportedLanguages.GO: 'go'
-}
+start = time.time()
 
 parser = argparse.ArgumentParser(description='Hazelcast Code Generator generates code of client protocol '
                                              'across languages.')
@@ -46,30 +19,29 @@ parser.add_argument('-l', '--lang',
                     dest='lang', action='store',
                     metavar='LANGUAGE', default='java',
                     choices=[lang.value for lang in SupportedLanguages],
-                    type=str, help='Language to generate codecs for (default is Java)')
+                    type=str, help='Language to generate codecs for (default is java)')
 
 args = parser.parse_args()
+lang_str = args.lang
+root_dir_arg = args.root_dir
+lang = SupportedLanguages[lang_str.upper()]
 
-start = time.time()
+root_dir = root_dir_arg if root_dir_arg is not None else './output/' + lang_str
+output_dir = root_dir + output_directories[lang]
 
-root = args.root_dir if args.root_dir is not None else './output/' + args.lang
-lang = args.lang
-output_dir = root + output_directories[SupportedLanguages[lang.upper()]]
-
-# Pwd
-__dir__ = os.path.dirname(os.path.realpath(__file__))
-
+# PWD
 dir_path = os.path.dirname(os.path.realpath(__file__))
 protocol_defs_path = dir_path + '/protocol-definitions/'
+
 services = load_services(protocol_defs_path)
 
 env = create_environment(lang)
 
-codec_template = env.get_template("codec-template.%s.j2" % lang)
+codec_template = env.get_template("codec-template.%s.j2" % lang_str)
 
 for service in services:
     if "methods" in service:
-        generate_codecs(service, codec_template, output_dir, file_extensions[SupportedLanguages[lang.upper()]])
+        generate_codecs(service, codec_template, output_dir, file_extensions[lang])
 
 end = time.time()
 
--- util.py
+++ util.py
@@ -3,44 +3,7 @@ import yaml
 import re
 from jinja2 import Environment, PackageLoader
 from java import java_types_encode, java_types_decode
-
-
-def load_services(protocol_def_dir):
-    service_list = os.listdir(protocol_def_dir)
-    services = []
-    for service_file in service_list:
-        with open(protocol_def_dir + service_file, 'r') as file:
-            data = yaml.load(file, Loader=yaml.Loader)
-            services.append(data)
-    return services
-
-
-def save_file(file, content):
-    os.makedirs(os.path.dirname(file), exist_ok=True)
-    with open(file, 'w') as file:
-        file.writelines(content)
-
-
-def create_environment(lang):
-    env = Environment(loader=PackageLoader(lang, '.'))
-    env.trim_blocks = True
-    env.lstrip_blocks = True
-    env.keep_trailing_newline = False
-    env.filters["capital"] = capital
-    env.globals["to_upper_snake_case"] = to_upper_snake_case
-    env.globals["fixed_params"] = fixed_params
-    env.globals["var_size_params"] = var_size_params
-    env.globals["is_var_sized_list"] = is_var_sized_list
-    env.globals["is_var_sized_map"] = is_var_sized_map
-    env.globals["item_type"] = item_type
-    env.globals["key_type"] = key_type
-    env.globals["value_type"] = value_type
-    env.globals["lang_types_encode"] = java_types_encode
-    env.globals["lang_types_decode"] = java_types_decode
-    env.globals["lang_name"] = java_name
-
-    return env
-
+from enum import Enum
 
 FixedLengthTypes = [
     "boolean",
@@ -98,19 +61,19 @@ def generate_codecs(service, template, output_dir, extension):
         save_file(output_dir + capital(service["name"]) + capital(method["name"]) + 'Codec.' + extension, content)
 
 
-def item_type(param_type):
+def item_type(lang_name, param_type):
     if param_type.startswith("List_"):
-        return java_name(param_type.split('_', 1)[1])
+        return lang_name(param_type.split('_', 1)[1])
 
 
-def key_type(param_type):
+def key_type(lang_name, param_type):
     if param_type.startswith("Map_"):
-        return java_name(param_type.split('_', 2)[1])
+        return lang_name(param_type.split('_', 2)[1])
 
 
-def value_type(param_type):
+def value_type(lang_name, param_type):
     if param_type.startswith("Map_"):
-        return java_name(param_type.split('_', 2)[2])
+        return lang_name(param_type.split('_', 2)[2])
 
 
 def is_var_sized_list(param_type):
@@ -119,3 +82,80 @@ def is_var_sized_list(param_type):
 
 def is_var_sized_map(param_type):
     return param_type.startswith("Map_") and param_type not in FixedMapTypes
+
+
+def load_services(protocol_def_dir):
+    service_list = os.listdir(protocol_def_dir)
+    services = []
+    for service_file in service_list:
+        with open(protocol_def_dir + service_file, 'r') as file:
+            data = yaml.load(file, Loader=yaml.Loader)
+            services.append(data)
+    return services
+
+
+def save_file(file, content):
+    os.makedirs(os.path.dirname(file), exist_ok=True)
+    with open(file, 'w') as file:
+        file.writelines(content)
+
+
+class SupportedLanguages(Enum):
+    JAVA = 'java'
+    CPP = 'cpp'
+    CS = 'cs'
+    PY = 'py'
+    NODE = 'node'
+    GO = 'go'
+
+
+output_directories = {
+    SupportedLanguages.JAVA: '/hazelcast/src/main/java/com/hazelcast/client/impl/protocol/codec/',
+    SupportedLanguages.CPP: '/hazelcast/generated-sources/src/hazelcast/client/protocol/codec/',
+    SupportedLanguages.CS: '/Hazelcast.Net/Hazelcast.Client.Protocol.Codec/',
+    SupportedLanguages.PY: '/hazelcast/protocol/codec/',
+    SupportedLanguages.NODE: '/src/codec/',
+    SupportedLanguages.GO: '/internal/proto/'
+}
+
+file_extensions = {
+    SupportedLanguages.JAVA: 'java',
+    SupportedLanguages.CPP: 'cpp',  # TODO header files ?
+    SupportedLanguages.CS: 'cs',
+    SupportedLanguages.PY: 'py',
+    SupportedLanguages.NODE: 'ts',
+    SupportedLanguages.GO: 'go'
+}
+
+language_specific_funcs = {
+    'lang_types_encode': {
+        SupportedLanguages.JAVA: java_types_encode
+    },
+    'lang_types_decode': {
+        SupportedLanguages.JAVA: java_types_decode
+    },
+    'lang_name': {
+        SupportedLanguages.JAVA: java_name
+    }
+}
+
+
+def create_environment(lang):
+    env = Environment(loader=PackageLoader(lang.value, '.'))
+    env.trim_blocks = True
+    env.lstrip_blocks = True
+    env.keep_trailing_newline = False
+    env.filters["capital"] = capital
+    env.globals["to_upper_snake_case"] = to_upper_snake_case
+    env.globals["fixed_params"] = fixed_params
+    env.globals["var_size_params"] = var_size_params
+    env.globals["is_var_sized_list"] = is_var_sized_list
+    env.globals["is_var_sized_map"] = is_var_sized_map
+    env.globals["item_type"] = item_type
+    env.globals["key_type"] = key_type
+    env.globals["value_type"] = value_type
+    env.globals["lang_types_encode"] = language_specific_funcs['lang_types_encode'][lang]
+    env.globals["lang_types_decode"] = language_specific_funcs['lang_types_decode'][lang]
+    env.globals["lang_name"] = language_specific_funcs['lang_name'][lang]
+
+    return env
