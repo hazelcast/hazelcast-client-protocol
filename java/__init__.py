9b1db6be17a18369a67c5488700329ba5567e351
ivanthescientist <ivan.yaschishin@hazelcast.com>
2021-11-01 13:37:31 +0200

    Added custom HazelcastJsonValue codec

--- java/__init__.py
+++ java/__init__.py
@@ -66,6 +66,7 @@ _java_types_common = {
     "Schema": "com.hazelcast.internal.serialization.impl.compact.Schema",
     "FieldDescriptor": "com.hazelcast.internal.serialization.impl.compact.FieldDescriptor",
     "List_FieldDescriptor": "java.util.List<com.hazelcast.internal.serialization.impl.compact.FieldDescriptor>",
+    "HazelcastJsonValue": "com.hazelcast.core.HazelcastJsonValue",
 }
 
 _java_types_encode = {
