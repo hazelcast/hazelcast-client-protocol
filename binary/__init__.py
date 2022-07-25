358e9b7f8a5874d069ebd97a59f05dbcfdfaff38
Tomasz Gawęda <tomasz.gaweda@outlook.com>
2022-07-25 12:10:38 +0200

    Add new custom type

--- binary/__init__.py
+++ binary/__init__.py
@@ -71,6 +71,7 @@ CustomConfigTypes = [
     'AttributeConfig',
     'IndexConfig',
     'BitmapIndexOptions',
+    'BTreeIndexConfig',
     'MapStoreConfigHolder',
     'MerkleTreeConfig',
     'NearCacheConfigHolder',
