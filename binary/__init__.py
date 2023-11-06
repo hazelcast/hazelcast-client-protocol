34f11148f9c26e981188c76f76cfea77c7c03ab9
Jack <jack.green@hazelcast.com>
2023-11-06 11:06:19 +0000

    REVERT reformat

--- binary/__init__.py
+++ binary/__init__.py
@@ -6,84 +6,95 @@ FixSizedTypes = [
     "UUID",
 ]
 
-VarSizedTypes = ["byteArray", "longArray", "String", "Data", "SqlPage"]
-
-FixSizedEntryListTypes = [
-    "EntryList_Integer_UUID",
-    "EntryList_UUID_Long",
-    "EntryList_Integer_Long",
-    "EntryList_Integer_Integer",
-    "EntryList_Long_byteArray",
-    "EntryList_UUID_UUID",
-    "EntryList_UUID_List_Integer",
+VarSizedTypes = [
+    'byteArray',
+    'longArray',
+    'String',
+    'Data',
+    'SqlPage'
 ]
 
-FixSizedMapTypes = []
+FixSizedEntryListTypes = [
+    'EntryList_Integer_UUID',
+    'EntryList_UUID_Long',
+    'EntryList_Integer_Long',
+    'EntryList_Integer_Integer',
+    'EntryList_Long_byteArray',
+    'EntryList_UUID_UUID',
+    'EntryList_UUID_List_Integer',
+]
 
-FixSizedListTypes = ["List_Integer", "List_Long", "List_UUID"]
+FixSizedMapTypes = [
+]
+
+FixSizedListTypes = [
+    'List_Integer',
+    'List_Long',
+    'List_UUID'
+]
 
 FixSizedSetTypes = [
-    "Set_UUID",
+    'Set_UUID',
 ]
 
 CustomTypes = [
-    "Address",
-    "CacheEventData",
-    "DistributedObjectInfo",
-    "QueryCacheEventData",
-    "RaftGroupId",
-    "ScheduledTaskHandler",
-    "SimpleEntryView",
-    "WanReplicationRef",
-    "Xid",
-    "ErrorHolder",
-    "StackTraceElement",
-    "ClientBwListEntry",
-    "MemberInfo",
-    "MemberVersion",
-    "MCEvent",
-    "AnchorDataListHolder",
-    "PagingPredicateHolder",
-    "EndpointQualifier",
-    "SqlQueryId",
-    "SqlError",
-    "SqlColumnMetadata",
-    "JobAndSqlSummary",
-    "SqlSummary",
-    "CPMember",
-    "MigrationState",
-    "FieldDescriptor",
-    "Schema",
-    "HazelcastJsonValue",
+    'Address',
+    'CacheEventData',
+    'DistributedObjectInfo',
+    'QueryCacheEventData',
+    'RaftGroupId',
+    'ScheduledTaskHandler',
+    'SimpleEntryView',
+    'WanReplicationRef',
+    'Xid',
+    'ErrorHolder',
+    'StackTraceElement',
+    'ClientBwListEntry',
+    'MemberInfo',
+    'MemberVersion',
+    'MCEvent',
+    'AnchorDataListHolder',
+    'PagingPredicateHolder',
+    'EndpointQualifier',
+    'SqlQueryId',
+    'SqlError',
+    'SqlColumnMetadata',
+    'JobAndSqlSummary',
+    'SqlSummary',
+    'CPMember',
+    'MigrationState',
+    'FieldDescriptor',
+    'Schema',
+    'HazelcastJsonValue'
 ]
 
 CustomConfigTypes = [
-    "CacheSimpleEntryListenerConfig",
-    "EventJournalConfig",
-    "EvictionConfigHolder",
-    "HotRestartConfig",
-    "ListenerConfigHolder",
-    "AttributeConfig",
-    "IndexConfig",
-    "BitmapIndexOptions",
-    "BTreeIndexConfig",
-    "MapStoreConfigHolder",
-    "MerkleTreeConfig",
-    "NearCacheConfigHolder",
-    "NearCachePreloaderConfig",
-    "PredicateConfigHolder",
-    "QueryCacheConfigHolder",
-    "QueueStoreConfigHolder",
-    "RingbufferStoreConfigHolder",
-    "TimedExpiryPolicyFactoryConfig",
-    "DurationConfig",
-    "MergePolicyConfig",
-    "CacheConfigHolder",
-    "DataPersistenceConfig",
-    "Capacity",
-    "MemoryTierConfig",
-    "DiskTierConfig",
-    "TieredStoreConfig",
+    'CacheSimpleEntryListenerConfig',
+    'EventJournalConfig',
+    'EvictionConfigHolder',
+    'HotRestartConfig',
+    'ListenerConfigHolder',
+    'AttributeConfig',
+    'IndexConfig',
+    'BitmapIndexOptions',
+    'BTreeIndexConfig',
+    'MapStoreConfigHolder',
+    'MerkleTreeConfig',
+    'NearCacheConfigHolder',
+    'NearCachePreloaderConfig',
+    'PredicateConfigHolder',
+    'QueryCacheConfigHolder',
+    'QueueStoreConfigHolder',
+    'RingbufferStoreConfigHolder',
+    'TimedExpiryPolicyFactoryConfig',
+    'DurationConfig',
+    'MergePolicyConfig',
+    'CacheConfigHolder',
+    'DataPersistenceConfig',
+    'Capacity',
+    'MemoryTierConfig',
+    'DiskTierConfig',
+    'TieredStoreConfig',
     "PartitioningAttributeConfig",
     "WanConsumerConfigHolder",
     "WanCustomPublisherConfigHolder",
@@ -96,62 +107,53 @@ CustomConfigTypes = [
     "DiscoveryConfig",
     "DiscoveryStrategyConfig",
     "WanSyncConfig",
-    "ResourceDefinition",
 ]
 
 VarSizedEntryListTypes = [
-    "EntryList_String_String",
-    "EntryList_String_byteArray",
-    "EntryList_String_EntryList_Integer_Long",
-    "EntryList_Data_Data",
-    "EntryList_Data_List_Data",
+    'EntryList_String_String',
+    'EntryList_String_byteArray',
+    'EntryList_String_EntryList_Integer_Long',
+    'EntryList_Data_Data',
+    'EntryList_Data_List_Data',
 ]
 
 VarSizedMapTypes = [
-    "Map_String_String",
-    "Map_EndpointQualifier_Address",
-    "Map_String_Data",
+    'Map_String_String',
+    'Map_EndpointQualifier_Address',
+    'Map_String_Data',
 ]
 
 VarSizedListTypes = [
-    "List_byteArray",
-    "List_CacheEventData",
-    "List_CacheSimpleEntryListenerConfig",
-    "List_Data",
-    "List_DistributedObjectInfo",
-    "List_ListenerConfigHolder",
-    "List_AttributeConfig",
-    "List_IndexConfig",
-    "List_MemberInfo",
-    "List_QueryCacheConfigHolder",
-    "List_QueryCacheEventData",
-    "List_ScheduledTaskHandler",
-    "List_String",
-    "List_Xid",
-    "List_StackTraceElement",
-    "List_ClientBwListEntry",
-    "List_MCEvent",
-    "List_SqlColumnMetadata",
-    "List_JobAndSqlSummary",
-    "List_CPMember" "ListCN_Data",
-    "List_List_Data",
-    "List_FieldDescriptor",
-    "List_PartitioningAttributeConfig",
-    "List_WanCustomPublisherConfigHolder",
-    "List_WanBatchPublisherConfigHolder",
-    "List_SimpleEntryView",
+    'List_byteArray',
+    'List_CacheEventData',
+    'List_CacheSimpleEntryListenerConfig',
+    'List_Data',
+    'List_DistributedObjectInfo',
+    'List_ListenerConfigHolder',
+    'List_AttributeConfig',
+    'List_IndexConfig',
+    'List_MemberInfo',
+    'List_QueryCacheConfigHolder',
+    'List_QueryCacheEventData',
+    'List_ScheduledTaskHandler',
+    'List_String',
+    'List_Xid',
+    'List_StackTraceElement',
+    'List_ClientBwListEntry',
+    'List_MCEvent',
+    'List_SqlColumnMetadata',
+    'List_JobAndSqlSummary',
+    'List_CPMember'
+    'ListCN_Data',
+    'List_List_Data',
+    'List_FieldDescriptor',
+    'List_PartitioningAttributeConfig',
+    'List_WanCustomPublisherConfigHolder',
+    'List_WanBatchPublisherConfigHolder',
+    'List_SimpleEntryView',
 ]
 
-AllTypes = (
-    FixSizedTypes
-    + VarSizedTypes
-    + FixSizedEntryListTypes
-    + FixSizedMapTypes
-    + FixSizedListTypes
-    + FixSizedSetTypes
-    + CustomTypes
-    + CustomConfigTypes
-    + VarSizedEntryListTypes
-    + VarSizedMapTypes
-    + VarSizedListTypes
-)
+AllTypes = FixSizedTypes + VarSizedTypes + FixSizedEntryListTypes \
+           + FixSizedMapTypes + FixSizedListTypes + FixSizedSetTypes \
+           + CustomTypes + CustomConfigTypes + VarSizedEntryListTypes \
+           + VarSizedMapTypes + VarSizedListTypes
