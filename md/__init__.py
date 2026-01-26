a36487e69883598b778337a840c17f6d349611c7
Rob Swain <rob.swain@hazelcast.com>
2026-01-26 16:04:04 +0000

    copy edit

--- md/__init__.py
+++ md/__init__.py
@@ -7,17 +7,11 @@ the recommended way of doing it is using the `md-to-pdf` tool.
 The documentation of the tool can be seen on
 https://www.npmjs.com/package/md-to-pdf.
 
-To install the tool, run the following command.
+To install the tool:
 
 $ npm i -g md-to-pdf
 
-Since the produced markdown document can be quite large
-in width due to some tables, it is recommended to set
-the output PDF margin to a lower value than the default setting.
-
-It is recommended that the right and left margins should
-be less than or equal to "10mm". One can use the following command
-to generate the PDF output.
+To generate PDF output:
 
 $ md-to-pdf documentation.md --pdf-options '{"margin": "10mm", "printBackground": true}'
 """
