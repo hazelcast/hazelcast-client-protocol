0b531f31e6de0ba6371bd1be935e9335636a0357
Rob Swain <rob.swain@hazelcast.com>
2026-01-27 10:55:55 +0000

    move PDF instructions and copy edit

--- md/__init__.py
+++ md/__init__.py
@@ -1,21 +1,3 @@
-"""
-This template produces Github-flavored markdown documents.
-
-If you want to convert this markdown document to PDF,
-the recommended way of doing it is using the `md-to-pdf` tool.
-
-The documentation of the tool can be seen on
-https://www.npmjs.com/package/md-to-pdf.
-
-To install the tool:
-
-$ npm i -g md-to-pdf
-
-To generate PDF output:
-
-$ md-to-pdf documentation.md --pdf-options '{"margin": "10mm", "printBackground": true}'
-"""
-
 internal_services = {
     "MC",
 }
